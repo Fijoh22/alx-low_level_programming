dfc577afeb12d4259b422f21628a19a3ba8a7e1b
Precious fijoh <preciousfijoh@gmail.com>
2022-12-09 19:43:46 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	;while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
