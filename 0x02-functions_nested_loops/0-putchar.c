c7ded40b16ec6f61ec30b2f96a7399ead61d942c
Precious fijoh <preciousfijoh@gmail.com>
2022-12-13 07:39:27 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,24 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * main - print _putchar
+ * main - prints _putchar and a new line
  * is written by fijoh22
  * Return : 0
  */
 
 int main(void)
+
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
