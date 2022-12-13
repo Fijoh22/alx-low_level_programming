0754b031297028ee8284d71b32c5e5ec042bd090
Precious fijoh <preciousfijoh@gmail.com>
2022-12-12 20:58:40 -0800

    Lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+/**
+ * * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'A'; ch++)
+		putchar(ch):\;
+
+	putchar('\n');
+
+	return (0);
