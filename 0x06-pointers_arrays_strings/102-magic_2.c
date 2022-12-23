8db3c611c317aca4824885e5d79546055b985f6b
Precious fijoh <preciousfijoh@gmail.com>
2022-12-22 17:37:20 -0800

    C - More pointers, arrays and strings Advanced tasks

--- 0x06-pointers_arrays_strings/102-magic_2.c
+++ 0x06-pointers_arrays_strings/102-magic_2.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
-  
-  a[2] = 1024;
-  p = &n;
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -16,8 +25,9 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+a[2] = 98
+(p + 5) = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
